9de5395a5bc76163482d5df3db0bdf65b182a376
Maloy Kumar <70903728+bitsbytes-maker@users.noreply.github.com>
2022-10-08 20:54:35 -0400

    Add files via upload

---
+++ c++_code/100_200_epoch/nn_100/hls.app
@@ -0,0 +1,26 @@
+<project xmlns="com.autoesl.autopilot.project" name="nn" top="vectormatrix">
+    <includePaths/>
+    <libraryPaths/>
+    <Simulation>
+        <SimFlow name="csim" csimMode="0" lastCsimMode="0"/>
+    </Simulation>
+    <files xmlns="">
+        <file name="../weight4.txt" sc="0" tb="1" cflags=" -Wno-unknown-pragmas" csimflags=" -Wno-unknown-pragmas" blackbox="false"/>
+        <file name="../weight3.txt" sc="0" tb="1" cflags=" -Wno-unknown-pragmas" csimflags=" -Wno-unknown-pragmas" blackbox="false"/>
+        <file name="../weight2.txt" sc="0" tb="1" cflags=" -Wno-unknown-pragmas" csimflags=" -Wno-unknown-pragmas" blackbox="false"/>
+        <file name="../weight1.txt" sc="0" tb="1" cflags=" -Wno-unknown-pragmas" csimflags=" -Wno-unknown-pragmas" blackbox="false"/>
+        <file name="../weight0.txt" sc="0" tb="1" cflags=" -Wno-unknown-pragmas" csimflags=" -Wno-unknown-pragmas" blackbox="false"/>
+        <file name="../test.cpp" sc="0" tb="1" cflags=" -Wno-unknown-pragmas" csimflags=" -Wno-unknown-pragmas" blackbox="false"/>
+        <file name="../data_test.txt" sc="0" tb="1" cflags=" -Wno-unknown-pragmas" csimflags=" -Wno-unknown-pragmas" blackbox="false"/>
+        <file name="../bias4.txt" sc="0" tb="1" cflags=" -Wno-unknown-pragmas" csimflags=" -Wno-unknown-pragmas" blackbox="false"/>
+        <file name="../bias3.txt" sc="0" tb="1" cflags=" -Wno-unknown-pragmas" csimflags=" -Wno-unknown-pragmas" blackbox="false"/>
+        <file name="../bias2.txt" sc="0" tb="1" cflags=" -Wno-unknown-pragmas" csimflags=" -Wno-unknown-pragmas" blackbox="false"/>
+        <file name="../bias1.txt" sc="0" tb="1" cflags=" -Wno-unknown-pragmas" csimflags=" -Wno-unknown-pragmas" blackbox="false"/>
+        <file name="../bias0.txt" sc="0" tb="1" cflags=" -Wno-unknown-pragmas" csimflags=" -Wno-unknown-pragmas" blackbox="false"/>
+        <file name="nn/nnnnn.cpp" sc="0" tb="false" cflags="" csimflags="" blackbox="false"/>
+    </files>
+    <solutions xmlns="">
+        <solution name="solution1" status="active"/>
+    </solutions>
+</project>
+
---
+++ c++_code/nn_vector/nnn_vector_train.cpp
@@ -0,0 +1,242 @@
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <math.h>
+#include <vector>
+
+
+#define nhiddenneurons 50
+using namespace std;
+
+vector< vector<long double> > weigh0;
+vector< vector<long double> > weigh1;
+vector< vector<long double> > weigh2;
+vector< vector<long double> > weigh3;
+vector< vector<long double> > weigh4;
+vector<long double>  bia0(nhiddenneurons);
+vector<long double>  bia1(nhiddenneurons);
+vector<long double>  bia2(nhiddenneurons);
+vector<long double>  bia3(nhiddenneurons);
+vector<long double>  bia4(2);
+
+
+
+vector<vector<long double> > vectormatrix(vector<vector<long double> > a, vector<vector<long double> >b) {
+	int row1 = a.size();
+	int col1 = a[0].size();
+	int row2 = b.size();
+	int col2 = b[0].size();
+	vector<vector<long double> > res( row1 , vector<long double> (col2, 0.0));
+	//ofstream myfile ("example1.txt",ios::app);
+	for(int i=0; i<row1; i++) {
+		for(int j= 0; j<col2; j++) {
+
+			long double sum =0;
+
+			for(int k=0; k<col1; k++) {
+				sum+=a[i][k]*b[k][j];
+			}
+
+			res[i][j] = sum;
+
+		}
+	}
+
+
+
+
+	return res;
+
+}
+
+vector<vector<long double> > vectormatrixwithbias(vector<vector<long double> > a, vector<vector<long double> >b, vector<long double> c) {
+	int row1 = a.size();
+	int col1 = a[0].size();
+	int row2 = b.size();
+	int col2 = b[0].size();
+	vector<vector<long double> > res( row1 , vector<long double> (col2, 0.0));
+	
+	for(int i=0; i<row1; i++) {
+		for(int j= 0; j<col2; j++) {
+
+			long double sum =0;
+
+			for(int k=0; k<col1; k++) {
+				sum+=a[i][k]*b[k][j];
+			}
+
+			res[i][j] = sum+c[j];
+
+			if(res[i][j]<0) {
+				res[i][j]=0;
+			}
+
+
+		}
+
+	}
+
+
+
+
+	return res;
+
+}
+
+
+vector<vector<long double> > transPosematrix(vector<vector<long double> > t) {
+	int row = t.size();
+	int col = t[0].size();
+	vector<vector<long double> > res( col , vector<long double> (row, 0));
+
+	for(int i=0; i<col; i++) {
+		for(int j=0; j<row; j++) {
+			res[i][j] = t[j][i];
+		}
+	}
+
+
+	return res;
+
+
+}
+
+
+
+void biasassignment(int r, vector<long double>  bia )
+{
+	for(int i =0;i<r;i++)
+	{
+	   bia[i] = 0;
+	}
+	
+}
+
+
+void assignment()
+{
+	biasassignment(nhiddenneurons,bia0);
+	biasassignment(nhiddenneurons,bia1);
+	biasassignment(nhiddenneurons,bia2);
+	biasassignment(nhiddenneurons,bia3);
+	biasassignment(2,bia4);
+	
+}
+
+
+vector<vector<long double> > Load_State(string file_name) {
+	ifstream in_state_(file_name.c_str(), ifstream::in);
+	vector< vector<long double > > state_out;
+	string start;
+
+	while (getline(in_state_, start)) {
+
+		vector<long double> x_coord;
+
+
+		istringstream ss(start);
+		long double a;
+		ss >> a;
+		x_coord.push_back(a);
+
+		string value;
+
+		while (getline(ss, value, '\t')) {
+			long double b;
+			ss >> b;
+			x_coord.push_back(b);
+		}
+
+		state_out.push_back(x_coord);
+	}
+
+	return state_out;
+}
+
+
+void shape(vector<vector<long double> > b) {
+	cout<<"shape\n";
+	cout<<b.size()<<endl;
+	cout<<b[0].size()<<endl;
+}
+
+long double scaling_std(long double max, long double min, long double values) {
+	long double std;
+	std = (values-min)/(max-min);
+
+
+	return std;
+
+}
+
+
+
+int main() {
+
+	vector< vector<long double> > data = Load_State("mixed200222.txt");
+
+
+	/// weight matrix loading here as per the gaussian distribution
+
+	weigh0 = Load_State("weight0.txt");
+	weigh1 = Load_State("weight1.txt");
+	weigh2 = Load_State("weight2.txt");
+	weigh3 = Load_State("weight3.txt");
+	weigh4 = Load_State("weight4.txt");
+
+
+
+	shape(weigh0);
+	shape(weigh1);
+	shape(weigh2);
+	shape(weigh3);
+	shape(weigh4);
+
+	vector< vector<long double> > features;
+
+	vector<int> labels;
+
+	long double Max[10] = {-100,137.000,197.000000 ,27.000000 ,2.000000 ,27.000000,2.000000,0.125365,0.473688,0.044516};
+	long double Min[10] = {-100,10.000000,13.000000,2.000000,0.000000,2.000000,0.000000,0.005385,0.027911,0.001095};
+
+//No.	Nodes	Edges	MaximumIndegree	MinimumIndegree	MaximumOutdegree
+//MinimumOutdegree	MedianPagerank	MaximumPagerank	MininumPagerank	StdPageRank	VarPageRank	State
+
+
+	for (int i = 0; i<data.size(); i++) {
+		vector<long double> interfeature;
+		for(int j=1; j<10; j++) {
+			interfeature.push_back(scaling_std(Max[j],Min[j],data[i][j]));
+		}
+		//interfeature.push_back(1.00);
+		features.push_back(interfeature);
+
+
+		if (data[i][12] > 0) {
+			labels.push_back(1);
+		} else {
+			labels.push_back(0);
+		}
+
+
+	}
+
+
+	cout<<features.size()<<"\n";
+	cout<<features[0].size()<<endl;
+
+
+	for (int i = 0; i<10; i++) {
+		for(int j=0; j<features[i].size(); j++) {
+			cout<<features[i][j] <<" ";
+		}
+		cout<<"\n";
+	}
+	
+	
+	assignment();
+
+
+	return 0;
+
+}
